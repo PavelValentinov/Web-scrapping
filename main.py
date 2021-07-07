05bb407a40fd6724c58a23b6eebb7a3b95b6f08e
Pavel Valentinov <hask90@gmail.com>
2021-07-07 17:49:30 +0300

    bug off

--- main.py
+++ main.py
@@ -1,15 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
 
-# получаем свой ip
-# response = requests.get('https://2ip.ru')
-# if not response.ok:
-#     raise RuntimeError('сайт не доступен')
-#
-# text = response.text
-#
-# soup = BeautifulSoup(text, features='html.parser')
-# print(soup.find('div', id='d_clip_button').text.strip())
 DESIRED_HUBS = {'Как'}
 KEYWORDS = ['проекта', 'Как']
 
@@ -21,19 +12,12 @@ text = response.text
 
 soup = BeautifulSoup(text, features='html.parser')
 articles = soup.find_all('article')
-# print(articles)
 for article in articles:
-    # hubs = {h.text.strip() for h in article.find_all('a', class_='hub-link')}
-    # if hubs & DESIRED_HUBS:
-    #     a = article.find('a', class_='post__title_link')
-    #     print(a.attrs.get('href'))
     hubs = []
     for h in article.find_all():
         hubs.append(h.text.strip())
-    # print(hubs)
 
     for word in KEYWORDS:
-        # full_hubs = ' '.join(hubs)
         for h in hubs:
             if word in h.split():
                 time = article.find(class_='post__time')
@@ -41,6 +25,3 @@ for article in articles:
                 href = article.find('a', class_='post__title_link')
                 # print('слово', h.split(), 'есть')
                 print(time.text, title.text, href.attrs.get('href'), '\n')
-
-
-# print(len(articles))
